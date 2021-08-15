94f45ce50e989e03631f2d2b2756429927ca97cd
Siamak <siamakmortaz125@gmail.com>
2021-08-15 12:51:38 +0430

    create model

--- post/models.py
+++ post/models.py
@@ -1,23 +1,34 @@
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
-class PostModel(models.Model):
-    title = models.CharField(max_length=200, verbose_name="عنوان")
-    body = RichTextField(blank=True, null=True, verbose_name="کامنت")
-    post_date = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name="blog_posts")
-
-    def total_likes(self):
-        return self.likes.count()
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
 
 
-class comment(models.Model):
-    post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=200)
-    body = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return '%s - %s' % (self.post.title, self.name)
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+
+# class comment(models.Model):
+#     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comments')
+#     name = models.CharField(max_length=200)
+#     body = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return '%s - %s' % (self.post.title, self.name)
--- post/views.py
+++ post/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from post.form import PostForm
-from post.models import PostModel
+from post.models import Post
 
 
 def Post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = PostModel(title=form.cleaned_data['title'], body=form.cleaned_data['body'])
+            post = Post(title=form.cleaned_data['title'], body=form.cleaned_data['body'])
             post.save()
-            info_post = PostModel.objects.all()
+            info_post = Post.objects.all()
             body = info_post.body
             print(body)
             form = post
